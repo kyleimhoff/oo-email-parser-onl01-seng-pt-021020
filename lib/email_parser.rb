0dec3b2e2a9a883e3d1f046dd23febbda9aba47b
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-08 18:23:42 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,9 @@ class EmailParser
     @emails = emails
   end
   def parse
-    @emails
-  end 
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
 end
   
